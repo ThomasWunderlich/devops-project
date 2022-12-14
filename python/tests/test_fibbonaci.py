59f51f0a21f88b217e1f5626d7aff79eaac74672
Thomas Wunderlich <twunde@gmail.com>
2022-12-13 22:33:33 -0500

    Added simpler test to check for off by one errors
    I wanted to confirm that I hadn't miscounted the number of items in the
    longer sequence. By splitting this up, it gives a quick way to reason
    about the sequence while also having a longer sequence that should
    uncover potential issues with a bad algorithm

--- python/tests/test_fibbonaci.py
+++ python/tests/test_fibbonaci.py
@@ -10,7 +10,11 @@ class TestFibonacci(unittest.TestCase):
         # This should throw an exception
         self.assertRaises(Exception, fibonacci, -1)
 
-    def test_fibonacci_valid_sequence(self):
+    def test_fibonacci_valid_short_sequence(self):
+        fib_seq = [0, 1, 1]
+        self.assertEqual(fibonacci(3), fib_seq)
+
+    def test_fibonacci_valid_long_sequence(self):
         fib_seq = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811]
         self.assertEqual(fibonacci(29), fib_seq)
 
